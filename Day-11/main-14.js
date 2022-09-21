2c6d7f6eb72c9ab93f0787c68c265bd5bdac1ce6
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-22 00:37:26 +0600

    Added New Files

--- Day-11/main-14.js
+++ Day-11/main-14.js
@@ -7,14 +7,6 @@ var addItems = document.getElementById('addItems');
 
 
 
-
-// for( var item of items ){
-//     item.addEventListener('click', function(e){
-//         e.target.parentNode.removeChild(e.target);
-//     });
-//     item.style.margin = '15px 0';
-// }
-
 addItems.addEventListener('click', function(){
     var newList = document.createElement('li');
     newList.classList.add('items');
